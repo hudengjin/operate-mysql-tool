dacd8d28d1ae8550c68a69ecf4e96db081cd4eb6
huprince <huprince@163.com>
2021-04-27 14:59:45 +0800

    mysql tool add sql clause variable feature

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/huprince/operate-mysql-tool/driver"
 	log "github.com/huprince/operate-mysql-tool/log"
@@ -11,12 +13,15 @@ import (
 	_ "github.com/huprince/operate-mysql-tool/config"
 )
 
+type sliceFlags []string
+
 var q string
 var p string
 var o bool
 var path string
 var split string
 var f string
+var mysqlVar sliceFlags
 
 const (
 	opCreate = "create"
@@ -26,6 +31,7 @@ const (
 	opQuery  = "query"
 )
 
+
 func init() {
 	flag.StringVar(&q, "sql", "", "-sql=?, sql clause option: select, update, insert, delete, create")
 	flag.StringVar(&p, "op", "insert", "-op=?, operations: create, insert, update, delete, query")
@@ -33,11 +39,13 @@ func init() {
 	flag.StringVar(&path, "path", "./output.txt", "-path=?, select clause output file path: ./output.txt ")
 	flag.StringVar(&split, "split", ",", "-split=?, output file split char: , "+" \\t"+" ")
 	flag.StringVar(&f, "f", "", "-f=?, sql clause as input file: ./test.sql")
+	flag.Var(&mysqlVar, "mysqlvar", "-mysqlvar=today:2021-04-20, mysql sql clause variable define")
 }
 
 func main() {
 	flag.Parse()
 
+	mysqlVarMap := parseMysqlVarToMap(mysqlVar)
 	if q == "" && f == "" {
 		log.Logger.Info("Please input query or ddl sql clause!")
 		os.Exit(1)
@@ -53,12 +61,13 @@ func main() {
 	} else {
 		sql = q
 	}
-
 	if split == "t" {
 		split = "\t"
 	}
 
 	log.Logger.Sugar().Infof("Execute operate: %s ", p)
+
+	sql = processSqlVar(sql, mysqlVarMap)
 
 	log.Logger.Info(sql)
 
@@ -73,4 +82,38 @@ func main() {
 		log.Logger.Info("Invalid operate, please execute -f option looking for help!")
 		os.Exit(1)
 	}
+}
+
+
+func (s *sliceFlags) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *sliceFlags) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
+func parseMysqlVarToMap(mysqlVarSlice sliceFlags) map[string]string {
+
+	mysqlVarMap := make(map[string]string)
+
+	for _, v := range mysqlVarSlice {
+		tmp := strings.Split(v, ":")
+		mysqlVarMap[tmp[0]] = tmp[1]
+	} 
+	return mysqlVarMap
+} 
+
+// processSqlVar 处理 sql 语句中的变量
+// sql 语句变量格式 ${mysqlvar:varKey}
+func processSqlVar(sourceSql string, varMap map[string]string) string {
+	if len(varMap) == 0 {
+		return sourceSql
+	}
+	
+	for varKey, varValue := range varMap {
+		sourceSql = strings.ReplaceAll(sourceSql, "${mysqlvar:" + varKey + "}", varValue)
+	}
+	return sourceSql
 }
